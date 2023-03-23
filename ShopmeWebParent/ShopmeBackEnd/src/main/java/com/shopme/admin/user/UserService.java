1bfb8d04a4fc851f3d0c8517f5d0842c940a34fe
Shashwat0212 <shashwat0212@gmail.com>
2023-03-24 00:28:22 +0530

    9.94% of 84hrs (24-03-2023)

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +19,14 @@ import jakarta.transaction.Transactional;
 @Service
 @Transactional
 public class UserService {
+	public static final int USERS_PER_PAGE = 4;
+
 	@Autowired
 	private UserRepository userRepo;
+
 	@Autowired
 	private RoleRepository roleRepo;
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
@@ -26,11 +34,19 @@ public class UserService {
 		return (List<User>) userRepo.findAll();
 	}
 
+	public Page<User> listByPage(int pageNum, String sortField,
+			String sortDir) {
+		Sort sort = Sort.by(sortField);
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+		Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE, sort);
+		return userRepo.findAll(pageable);
+	}
+
 	public List<Role> listRoles() {
 		return (List<Role>) roleRepo.findAll();
 	}
 
-	public void save(User user) {
+	public User save(User user) {
 		boolean isUpdatingUser = (user.getId() != null);
 		if (isUpdatingUser) {
 			User existingUser = userRepo.findById(user.getId()).get();
@@ -42,7 +58,7 @@ public class UserService {
 		} else {
 			encodePassword(user);
 		}
-		userRepo.save(user);
+		return userRepo.save(user);
 	}
 
 	private void encodePassword(User user) {
@@ -70,14 +86,16 @@ public class UserService {
 		try {
 			return userRepo.findById(id).get();
 		} catch (NoSuchElementException ex) {
-			throw new UserNotFoundException("Could not find any user wth ID " + id);
+			throw new UserNotFoundException(
+					"Could not find any user wth ID " + id);
 		}
 	}
 
 	public void delete(Integer id) throws UserNotFoundException {
 		Long countById = userRepo.countById(id);
 		if (countById == null || countById == 0) {
-			throw new UserNotFoundException("Could not find any user wth ID " + id);
+			throw new UserNotFoundException(
+					"Could not find any user wth ID " + id);
 		}
 		userRepo.deleteById(id);
 	}
@@ -85,5 +103,4 @@ public class UserService {
 	public void updateUserEnabledStatus(Integer id, boolean enabled) {
 		userRepo.updateEnabledStatus(id, enabled);
 	}
-
 }
