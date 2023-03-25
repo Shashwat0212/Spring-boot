26d4c5990c7162ef442188d39af9630ba6435279
Shashwat0212 <shashwat0212@gmail.com>
2023-03-25 23:57:04 +0530

    11.6% of 84hrs (25-03-2023)

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -20,6 +20,8 @@ import com.shopme.admin.FileUploadUtil;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @Controller
 public class UserController {
 	@Autowired
@@ -27,16 +29,17 @@ public class UserController {
 
 	@GetMapping("/users")
 	public String listFirstPage(Model model) {
-		return listByPage(1, model, "firstName", "asc");
+		return listByPage(1, model, "firstName", "asc", null);
 	}
 
 	@GetMapping("/users/page/{pageNum}")
 	public String listByPage(@PathVariable(name = "pageNum") int pageNum,
 			Model model, @Param("sortField") String sortField,
-			@Param("srtDir") String sortDir) {
+			@Param("srtDir") String sortDir, @Param("keyword") String keyword) {
 		System.out.println("SortField: " + sortField);
 		System.out.println("Sort Dir: " + sortDir);
-		Page<User> page = service.listByPage(pageNum, sortField, sortDir);
+		Page<User> page = service.listByPage(pageNum, sortField, sortDir,
+				keyword);
 		List<User> listUsers = page.getContent();
 		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
 		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
@@ -49,10 +52,11 @@ public class UserController {
 		model.addAttribute("currentPage", pageNum);
 		model.addAttribute("startCount", startCount);
 		model.addAttribute("endCount", endCount);
-		model.addAttribute("totalItems", page.getNumberOfElements());
+		model.addAttribute("totalItems", page.getTotalElements());
 		model.addAttribute("listUsers", listUsers);
 		model.addAttribute("sortField", sortField);
 		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("keyword", keyword);
 		model.addAttribute("reverseSortDir", reverseSortDir);
 		return "users";
 	}
@@ -85,10 +89,15 @@ public class UserController {
 				user.setPhotos(null);
 			service.save(user);
 		}
-		// service.save(user);
 		redirectAttributes.addFlashAttribute("message",
 				"The user has been saved successfully.");
-		return "redirect:/users";
+		return getRedirectURLToAffectedUser(user);
+	}
+
+	private String getRedirectURLToAffectedUser(User user) {
+		String firstPartOfMail = user.getEmail().split("@")[0];
+		return "redirect:/users/page/1?sortField=id&sortDir=asc&keyword="
+				+ firstPartOfMail;
 	}
 
 	@GetMapping("/users/edit/{id}")
@@ -129,5 +138,18 @@ public class UserController {
 		String message = "The user ID " + id + " has been " + status;
 		redirectAttributes.addFlashAttribute("message", message);
 		return "redirect:/users";
+	}
+
+	@GetMapping("/users/export/csv")
+	public void exportToCSV(HttpServletResponse response) throws IOException {
+		List<User> listUsers = service.listAll();
+		UserCsvExporter exporter = new UserCsvExporter();
+		exporter.export(listUsers, response);
+	}
+	@GetMapping("/users/export/excel")
+	public void exportToExcel(HttpServletResponse response) throws IOException {
+		List<User> listUsers = service.listAll();
+		UserExcelExporter exporter = new UserExcelExporter();
+		exporter.export(listUsers, response);
 	}
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -1,7 +1,7 @@
 package com.shopme.admin.user;
 
-import com.shopme.common.entity.Role;
-import com.shopme.common.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -9,15 +9,23 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.shopme.common.entity.User;
+
 @Repository
 public interface UserRepository
-  extends PagingAndSortingRepository<User, Integer>, CrudRepository<User, Integer> {
-  @Query("SELECT u FROM User u WHERE u.email = :email")
-  public User getUserByEmail(@Param("email") String email);
+		extends
+			PagingAndSortingRepository<User, Integer>,
+			CrudRepository<User, Integer> {
+	@Query("SELECT u FROM User u WHERE u.email = :email")
+	public User getUserByEmail(@Param("email") String email);
 
-  public Long countById(Integer id);
+	public Long countById(Integer id);
 
-  @Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
-  @Modifying
-  public void updateEnabledStatus(Integer id, boolean enabled);
+	@Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstName, ' ', "
+			+ "u.lastName) LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
+
+	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
+	@Modifying
+	public void updateEnabledStatus(Integer id, boolean enabled);
 }
