c1b49c8ebe52ab717a0bcce0fe7d53227744db12
Shashwat0212 <shashwat0212@gmail.com>
2023-04-18 23:59:35 +0530

    33.3% of 84hrs(130/310) (18-04-2023)

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/ShopmeFrontEndApplication.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/ShopmeFrontEndApplication.java
@@ -1,9 +1,11 @@
-package com.shopme.site;
+package com.shopme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 public class ShopmeFrontEndApplication {
 
 	public static void main(String[] args) {
